4e6e4745aa99199460b93f855d58aab1822fe805
Zee <amurphey7@gmail.com>
2019-10-22 12:28:25 -0400

    Documentation

--- src/com/aidanmurphey/decimaltobinary/Converter.java
+++ src/com/aidanmurphey/decimaltobinary/Converter.java
@@ -6,12 +6,20 @@ public class Converter {
     
     private int decimal;
     private Stack<Integer> stack;
-    
+
+    /**
+     * Constructor for Converter class
+     * @param decimal The decimal integer to be converted
+     */
     public Converter(int decimal){
         this.decimal = decimal;
         stack = new Stack();
     }
-    
+
+    /**
+     * Converts the supplied decimal in the constructor to binary and returns the binary as a string
+     * @return The supplied decimal integer converted to binary
+     */
     public String decimalToBinary() {
         decimalToBinary(this.decimal);
         
@@ -22,7 +30,12 @@ public class Converter {
         
         return builder.toString();
     }
-    
+
+    /**
+     * Uses recursion to convert a given decimal integer to binary. Uses the class's stack member variable.
+     * Called by the other decimalToBinary method
+     * @param decimal The supplied decimal integer to be converted to binary
+     */
     private void decimalToBinary(int decimal) {
         int q = decimal / 2;
         int r = decimal % 2;
